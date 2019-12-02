3070030dee03fc5380c2f56cc0e7fc5d98cc64e3
nsurvance <58082491+nsurvance@users.noreply.github.com>
2019-12-02 13:28:08 -0500

    Add files via upload

--- javascript/parallax.js
+++ javascript/parallax.js
@@ -1,6 +1,14 @@
 const parallax = document.getElementById("parallax");
 
+
 window.addEventListener("scroll", function () {
     let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = offset * -.2 + "px";
+    let width = document.body.clientWidth;
+
+    if (width >= 500) {
+        parallax.style.backgroundPositionY = offset * -.2 + "px";
+    } else {
+        parallax.style.backgroundPositionY = offset * -.05 + "px";
+    }
+
 })
